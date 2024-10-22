23c779de9716870fd1f2a85a7d8f16d0e1341fed
Casey Wittrock <caseywittrockmn@gmail.com>
2024-10-21 21:29:36 -0500

    initial commit

---
+++ src/GameApi.js
@@ -0,0 +1,10 @@
+import companies from "./companies/companies";
+const fs = require("fs");
+
+class GameApi {
+  static companies = companies;
+
+  static Manipulate() {}
+  static readGames() {}
+  static saveGame() {}
+}
---
+++ src/MarketManipulators.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MarketManipulators = () => {
+  return <div>MarketManipulators</div>;
+};
+
+export default MarketManipulators;
